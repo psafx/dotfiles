5dd07e26ddb17090027e31dfb5af4ab92094301e
Prasong Aroonruviwat <psafx@hotmail.com>
2014-03-04 16:15:11 +0700

    update tomorrow theme

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,9 @@ ln -sf ../dotfiles/.vim/syntax .
 ln -sf ../dotfiles/.vim/colors .
 
 mkdir backup tmp undo
+
+mkdir -p ~/.vim/autoload
+curl -so ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
+
+mkdir -p ~/.vim/bundle
+git clone https://github.com/myusuf3/numbers.vim.git ~/.vim/bundle/numbers
